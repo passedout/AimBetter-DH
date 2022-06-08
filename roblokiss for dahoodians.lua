d788cbfddc68875b5c03a411bb5e2a009fc1376a
! ◝漢Brooklyn漢◜ <94296856+GotRipped@users.noreply.github.com>
2022-06-09 01:01:09 +0200

    Update roblokiss for dahoodians.lua

--- roblokiss for dahoodians.lua
+++ roblokiss for dahoodians.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/GotRipped/BetterAim.Lua-DH/main/AimBetter.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GotRipped/Aim-BetterDH/main/Aim.lua", true))()
